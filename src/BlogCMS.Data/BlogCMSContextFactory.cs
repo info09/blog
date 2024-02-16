6de50ef5923acd4388827d2c49efd1fa591e05c2
VG-AMBER-HUYTQ\Tran Quang Huy <huytq@vega.com.vn>
2024-02-16 15:56:42 +0700

    migration database

---
+++ src/BlogCMS.Data/BlogCMSContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace BlogCMS.Data
+{
+    public class BlogCMSContextFactory : IDesignTimeDbContextFactory<BlogCMSContext>
+    {
+        public BlogCMSContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<BlogCMSContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new BlogCMSContext(builder.Options);
+        }
+    }
+}
