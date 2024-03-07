037359788060549866ad1a10bf1faaf70a942bf9
VG-AMBER-HUYTQ\Tran Quang Huy <huytq@vega.com.vn>
2024-03-07 17:16:17 +0700

    profile và update profile

---
+++ src/BlogCMS.WebApp/Controllers/ProfileController.cs
@@ -0,0 +1,84 @@
+﻿using BlogCMS.Core.Domain.Identity;
+using BlogCMS.Core.SeedWorks;
+using BlogCMS.Core.SeedWorks.Constants;
+using BlogCMS.WebApp.Extensions;
+using BlogCMS.WebApp.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCMS.WebApp.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public ProfileController(IUnitOfWork unitOfWork, SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _unitOfWork = unitOfWork;
+        }
+
+        [Route("profile")]
+        public async Task<IActionResult> Index()
+        {
+            var userId = User.GetUserId();
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+            return View(new ProfileViewModel()
+            {
+                FirstName = user.FirstName,
+                Email = user.Email,
+                LastName = user.LastName
+            });
+        }
+
+        [HttpGet("profile/edit")]
+        public async Task<IActionResult> ChangeProfile()
+        {
+            var userId = User.GetUserId();
+            var user = await _userManager.FindByIdAsync($"{userId}");
+            return View(new ChangeProfileViewModel()
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+            });
+        }
+
+        [Route("/profile/edit")]
+        [HttpPost]
+        public async Task<IActionResult> ChangeProfile([FromForm] ChangeProfileViewModel model)
+        {
+            var userId = User.GetUserId();
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                TempData["Success"] = "Update profile thành công.";
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Update profile failed");
+
+            }
+            return View(model);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            await HttpContext.SignOutAsync();
+
+            return Redirect(UrlConstants.Home);
+        }
+    }
+}
